183f5fb09732c336c463861b324d1ce900e3a966
Bamvor Jian ZHANG <bamv2005@gmail.com>
2021-01-07 16:28:54 +0800

    Return to user space
    Signed-off-by: Bamvor Jian ZHANG <bamv2005@gmail.com>

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 
+CROSS_COMPILE=/home/bamvor/works/source2/software/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin//riscv64-unknown-elf-
 
 all:
-	riscv64-unknown-elf-gcc -c -mcmodel=medany -march=rv64imafdc -mabi=lp64d -x assembler-with-cpp -ggdb -c -o head.o head.S
-	riscv64-unknown-elf-gcc -c -mcmodel=medany -march=rv64imafdc -mabi=lp64d -ggdb -c -o cpu_entry.o cpu_entry.c -nostdlib
-	riscv64-unknown-elf-gcc -o xuxiake.elf -mcmodel=medany -march=rv64imafdc -mabi=lp64d -nostartfiles -Wl,--gc-sections,-Map=xuxiake.map,-cref,-u,_start -T link.lds head.o cpu_entry.o
-	riscv64-unknown-elf-objcopy -O binary xuxiake.elf xuxiake.bin
+	${CROSS_COMPILE}gcc -c -mcmodel=medany -march=rv64imafdc -mabi=lp64d -x assembler-with-cpp -ggdb -c -o head.o head.S
+	${CROSS_COMPILE}gcc -c -mcmodel=medany -march=rv64imafdc -mabi=lp64d -ggdb -c -o cpu_entry.o cpu_entry.c -nostdlib
+	${CROSS_COMPILE}gcc -o xuxiake.elf -mcmodel=medany -march=rv64imafdc -mabi=lp64d -nostartfiles -Wl,--gc-sections,-Map=xuxiake.map,-cref,-u,_start -T link.lds head.o cpu_entry.o
+	${CROSS_COMPILE}objcopy -O binary xuxiake.elf xuxiake.bin
 
 qemu:
 	qemu-system-riscv64 -M virt -m 512M -smp 1 -bios /home/bamvor/works/source/opensbi/build/platform/generic/firmware/fw_jump.bin -kernel xuxiake.bin -nographic
--- cpu_entry.c
+++ cpu_entry.c
@@ -2,6 +2,19 @@
 /* SBI Extension IDs */
 #define SBI_EXT_0_1_CONSOLE_PUTCHAR             0x1
 
+/* CSR definition */
+#define CSR_STVEC                       0x105
+#define CSR_MEPC                        0x341
+
+#define csr_write(csr, val)                                        \
+        ({                                                         \
+                unsigned long __v = (unsigned long)(val);          \
+                __asm__ __volatile__("csrw 0x105, %0" \
+                                     :                             \
+                                     : "rK"(__v)                   \
+                                     : "memory");                  \
+        })
+
 int sbi_ecall(int ext, unsigned long arg0)
 {
 	int ret;
@@ -22,12 +35,55 @@ void xxk_putchar(char c)
 	sbi_ecall(SBI_EXT_0_1_CONSOLE_PUTCHAR, c);
 }
 
+void xxk_print(char *s)
+{
+	while(*s)
+		xxk_putchar(*s++);
+}
+
+void exception_handler()
+{
+	xxk_print("exception occur!\n");
+}
+
+void setup_exception_vector()
+{
+        {
+                unsigned long __v = (unsigned long)(exception_handler);
+                __asm__ __volatile__("csrw 0x105, %0"
+                                     :
+                                     : "rK"(__v)
+                                     : "memory");
+        }
+}
+
+void user_program()
+{
+	xxk_print("try to output!\n");
+}
+
+void jump_to_user_mode()
+{
+        {
+                unsigned long __v = (unsigned long)(user_program);
+                __asm__ __volatile__("csrw 0x141, %0"
+                                     :
+                                     : "rK"(__v)
+                                     : "memory");
+        }
+	__asm__ __volatile__("sret" : : );
+	xxk_print("should not here!\n");
+	while(1);
+}
+
 void cpu_entry(void)
 {
-	char *output = "Hello XU Xiake";
-	while(*output)
-		xxk_putchar(*output++);
+	char *output = "Hello XU Xiake\n";
 
-	xxk_putchar('\n');
+	xxk_print(output);
+	setup_exception_vector();
+	jump_to_user_mode();
+	xxk_print("exit!\n");
+	while(1);
 }
 
