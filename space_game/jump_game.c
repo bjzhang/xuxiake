53f387c0c1b44b02a6b7296c5d4d41c809bc06fd
Bamvor ZHANG <bamv2005@gmail.com>
2021-05-18 18:52:32 +0800

    [WIP]: game
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

--- space_game/jump_game.c
+++ space_game/jump_game.c
@@ -5,13 +5,101 @@
 
 static void finish(int sig);
 
-int
-main(int argc, char *argv[])
+static void draw_window_border(WINDOW *win)
+{
+	wattron(win, COLOR_PAIR(4));
+	box(win, ACS_VLINE, ACS_HLINE);
+	wrefresh(win);
+}
+
+/**
+ *  jump from (start_x, start_y) with F jump and G.
+ *  a_jump = F_jump * mass;	//a = accelerated velocity
+ *  v_jump = A_jump * jump_interval;
+ *  G      = g * mass;
+ *  v_landing = G * t
+ *  y = (v_landing - v_jump) * t;
+ *
+ *  stop when y <= start_y
+ */
+static int jump_step(void)
+{
+	int mass = 1000;
+	int g = 10;
+	int F_jump = 100;
+	int a_jump = F_jump * mass;	//10 0000
+	int jump_interval = 10;
+	int v_jump = a_jump * jump_interval;	//100 0000
+	int G = g * mass;
+	int v_landing = 0;
+	static int time = 1;
+
+	v_landing = G * time; //100000
+	y = (v_landing - v_jump) * time;
+	time++;
+
+	return y;
+}
+
+static void jumper(int scr_max_y, int scr_max_x)
+{
+	WINDOW *main_win;
+	int y, x;
+	int ch;
+
+	y = scr_max_y - 1;
+	x = 0;
+	main_win= newwin(scr_max_y, scr_max_x, 0, 0);
+	//error handling.
+	draw_window_border(main_win);
+	wattron(main_win, COLOR_PAIR(2));
+	wmove(main_win, y, x);
+	wrefresh(main_win);
+	curs_set(0);	//hide the curser
+	for (;;) {
+		draw_window_border(main_win);
+		wattron(main_win, COLOR_PAIR(2));
+		if ((ch = getch()) != ERR) {
+			/* delete the previous char */
+			wmove(main_win, y, x);
+			waddch(main_win, ' ' | A_INVIS);
+			wrefresh(main_win);
+			switch(ch) {
+				case KEY_UP:
+					y--;
+					if (y <= 0)
+						y = 0;
+					break;
+				case KEY_DOWN:
+					y++;
+					if (y >= scr_max_y - 1)
+						y = scr_max_y -1;
+					break;
+				case KEY_LEFT:
+					x--;
+					if (x <= 0)
+						x = 0;
+					break;
+				case KEY_RIGHT:
+					x++;
+					if (x >= scr_max_x - 1)
+						x = scr_max_x - 1;
+					break;
+				case ' ':
+					int y = jump_step();
+					if (y < 
+					break;
+			}
+			wmove(main_win, y, x);
+			waddch(main_win, '#');
+			wrefresh(main_win);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
 {
 	int stdscr_h, stdscr_w;
-	WINDOW *main_win;
-	int ch;
-	int i;
 
 	/* initialize your non-curses data structures here */
 
@@ -22,6 +110,7 @@ main(int argc, char *argv[])
 	(void) nonl();         /* tell curses not to do NL->CR/NL on output */
 	(void) cbreak();       /* take input chars one at a time, no wait for \n */
 	(void) noecho();       /* no echo input */
+	nodelay(stdscr, TRUE);
 
 	if (has_colors())
 	{
@@ -47,19 +136,7 @@ main(int argc, char *argv[])
 	if (stdscr_h < 24 || stdscr_w < 80)
 		goto err;
 
-	main_win= newwin(24, 80, 0, 0);
-	i = 0;
-	wattron(main_win, COLOR_PAIR(2));
-	nodelay(stdscr, TRUE);
-	for (;;) {
-		if ((ch = getch()) != ERR) {
-			wmove(main_win, 24, i);
-			waddch(main_win, ch);
-			wrefresh(main_win);
-			i++;
-		}
-	}
-
+	jumper(24, 80);
 err:
 	finish(0);               /* we are done */
 }
--- test/atomic_test.c
+++ test/atomic_test.c
@@ -2,21 +2,74 @@
 
 #include <linux/module.h>
 #include <linux/kthread.h>
+#include <linux/delay.h>
+#include <asm/spinlock.h>
 
 #define NUM_OF_THREADS 2
 
 struct atomic_args {
-	spinlock_t lock;
 	int nr;		/* the sequence number os thread */
 };
 
 struct atomic_args atomic_args_array[NUM_OF_THREADS];
 struct task_struct *atomic_threads[NUM_OF_THREADS];
 
+static DEFINE_SPINLOCK(atomic_op);
+
+#define CONFIG_SMP
+/**
+ * From linux/arch/riscv/include/asm/fence.h
+ */
+#ifdef CONFIG_SMP
+#define RISCV_ACQUIRE_BARRIER		"\tfence r , rw\n"
+#define RISCV_RELEASE_BARRIER		"\tfence rw,  w\n"
+#else
+#define RISCV_ACQUIRE_BARRIER
+#define RISCV_RELEASE_BARRIER
+#endif
+/**
+ * From linux/arch/riscv/include/asm/fence.h end
+ */
+
+typedef struct {
+	volatile unsigned int lock;
+} xxk_arch_spinlock_t;
+
+/**
+ * OutputOperands:
+ * r A register operand is allowed provided that it is in a general register.
+ * https://gcc.gnu.org/onlinedocs/gcc/Simple-Constraints.html#Simple-Constraints
+ * A An address that is held in a general-purpose register.
+ * https://gcc.gnu.org/onlinedocs/gcc/Machine-Constraints.html#Machine-Constraints
+ * = Means that this operand is written to by this instruction: the previous value is discarded and replaced by new data.
+ * https://gcc.gnu.org/onlinedocs/gcc/Modifiers.html#Modifiers
+ */
+static inline int xxk_arch_spin_trylock(xxk_arch_spinlock_t *lock)
+{
+	int tmp = 1, busy;
+
+	__asm__ __volatile__ (
+		"	amoswap.w %0, %2, %1\n"
+		RISCV_ACQUIRE_BARRIER
+		: "=r" (busy), "+A" (lock->lock)
+		: "r" (tmp)
+		: "memory");
+
+	return !busy;
+}
 static int atomic_test(void *arg)
 {
 	struct atomic_args *th = (struct atomic_args*)arg;
 	pr_info("thread %d\n", th->nr);
+
+	set_current_state(TASK_INTERRUPTIBLE);
+	while (!kthread_should_stop()) {
+		arch_spin_lock(&atomic_op.rlock.raw_lock);
+		pr_info("thread %d get lock\n", th->nr);
+		arch_spin_unlock(&atomic_op.rlock.raw_lock);
+		usleep_range(1000000, 1000001);
+	}
+	__set_current_state(TASK_RUNNING);
 
 	return 0;
 }
@@ -53,6 +106,9 @@ static void __exit atomic_exit(void)
 {
 	int i;
 
+	for (i = 0; i< NUM_OF_THREADS; i++) {
+		kthread_stop(atomic_threads[i]);
+	}
 	pr_info("byebye %s\n", __func__);
 }
 
