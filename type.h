0942e1f2e474508c70d6b229128a83391c68c523
Bamvor ZHANG <bamv2005@gmail.com>
2022-10-31 12:31:48 +0800

    WIP: Add missing files for previous commit
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

---
+++ type.h
@@ -0,0 +1,12 @@
+#ifndef __TYPE_H
+#define __TYPE_H
+typedef unsigned long	u64;
+typedef long		s64;
+typedef unsigned int	u32;
+typedef int		s32;
+typedef unsigned char	u8;
+typedef signed char	s8;
+
+#define NULL		((void*)0)
+
+#endif /* #ifdef __TYPE_H */
